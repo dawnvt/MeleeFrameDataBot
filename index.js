fb522a0897f44b1bda996aa68b65d89b77e1c81d
dawnvt <stablumtrym@gmail.com>
2021-02-14 16:25:05 +0100

    Commandhandler

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ require('dotenv').config()
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+/// Imports .env vars
+const token = process.env.discord_token
+const prefix = process.env.prefix
+
+/// Default Discord.JS example logging code
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+/// Commandhandler - Handles prefix usage
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+  if (!msg.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
 });
 
-client.login(process.env.discord_token);
+/// Default Discord.JS example log-on code
+client.login(token);
